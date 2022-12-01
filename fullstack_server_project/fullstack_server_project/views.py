3bd00cec318c2f98bc72c471d919ce9b84de468b
lilyyang93 <42586955+lilyyang93@users.noreply.github.com>
2022-11-30 15:19:11 -1000

    fake whoami route; works

--- fullstack_server_project/fullstack_server_project/views.py
+++ fullstack_server_project/fullstack_server_project/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
 
 def index(request):
     print('index')
     homepage = open('static/index.html').read()
     return HttpResponse(homepage)
+
+@api_view(['GET'])
+def whoami(request):
+    print("whoami")
+    user = {"name": "Lily", "email": "lily@codeplatoon.com"}
+    return JsonResponse(data=user)
